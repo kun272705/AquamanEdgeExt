e33045b71ad26179d67fa17a9a55d1e902604ea5
Kun Fang <kun272705@outlook.com>
2026-02-04 14:03:13 +0800

    Finished the framework

--- settings.js
+++ settings.js
@@ -1,6 +1,4 @@
 
-import { DouyinChannelDownloader } from './agents/douyin-channel-downloader/douyin-channel-downloader.js';
-
 export class settings {
 
   static extName = 'Aquaman Edge Ext';
@@ -9,10 +7,5 @@ export class settings {
 
   static nativeMessagingHostName = 'com.kun272705.aquaman';
       
-  static agentRegistry = [
-    {
-      'patterns': ["https://www.douyin.com/user/*"],
-      'class': DouyinChannelDownloader
-    }
-  ];
+  static agentRegistry = [];
 };
